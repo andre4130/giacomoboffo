04a71252f99d94839010f5da3b4e1829419ff27b
andre <andre4130@gmail.com>
2021-02-12 15:59:20 +0100

    added latest files

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,162 +1,173 @@
 export const projects = [
-    {
-        "id": "1",
-        "image": require("../assets/Giacomo-Boffo-Podere-Tranche-1.jpg"),
-        "url": "",
-        "slug": "Podere Tranche",
-        "description": "Brand Identity for Podere Tranche winery",
-        "web": "false"
-    },
-    // {
-    //     "id": "2",
-    //     "image": require("../assets/GiacomoBoffo_NachoAlegre_1.jpg"),
-    //     "url": "http://nachoalegre.com/",
-    //     "slug": "Nacho Alegre",
-    //     "description": "<a href='http://nachoalegre.com/' target='_blank'>Website</a> &nbsp; for photographer Nacho Alegre. At Apartamento Studios.",
-    //     "web": "true"
-    // },
-    {
-        "id": "2",
-        "image": require("../assets/GiacomoBoffo_NachoAlegre_1.jpg"),
-        "url": "http://nachoalegre.com/",
-        "slug": "Nacho Alegre",
-        "description": "Website for photographer Nacho Alegre. At Apartamento Studios.",
-        "web": "true"
-    },
-    {
-        "id": "3",
-        "image": require("../assets/GiacomoBoffo_BarcelonaDesignWeek_1.jpg"),
-        "url": "",
-        "slug": "Barcelona Design Week",
-        "description": "Campaign for Barcelona Design Week 2018. Illustration Bráulio Amado, photo Leonardo García Méndez. At Folch",
-        "web": "false"
-    },
-    // {
-    //     "id": "4",
-    //     "image": require("../assets/Giacomo-Boffo-Brand-XY+.jpg"),
-    //     "url": "https://www.lascuolaopensource.xyz/en",
-    //     "slug": "Giacomo-Boffo-Brand-XY+",
-    //     "description": "Campaign for workshop ‘Un Brand per XY+’ at &nbsp;<a href='https://www.lascuolaopensource.xyz/en' target='_blank'> La Scuola Open Source</a>",
-    //     "web": "true"
-    // },
-    {
-        "id": "4",
-        "image": require("../assets/Giacomo-Boffo-Brand-XY+.jpg"),
-        "url": "https://www.lascuolaopensource.xyz/en",
-        "slug": "Giacomo-Boffo-Brand-XY+",
-        "description": "Campaign for workshop ‘Un Brand per XY+’ at La Scuola Open Source",
-        "web": "true"
-    },
-    {
-        "id": "5",
-        "image": require("../assets/Giacomo-Boffo-Hermanos-Vinagre-1.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-Hermanos-Vinagre-1",
-        "description": "Brand identity for seafood tapas bar Hermanos Vinagre in Madrid. Illustrations Hunter French. At Apartamento",
-        "web": "false"
-    },
-    {
-        "id": "6",
-        "image": require("../assets/Giacomo-Boffo-Manta-Poster.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-Manta-Poster",
-        "description": "Naming, logo and identity proposal for the former Manifattura Tabacchi in Cagliari. With Arianna Smaron",
-        "web": "false"
-    },
-    {
-        "id": "7",
-        "image": require("../assets/Giacomo-Boffo-Valentina-Barucci-1.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-Valentina-Barucci-1",
-        "description": "Campaign for exhibition ‘Valentina Barucci: Opere 1980-2020’",
-        "web": "false"
-    },
-    {
-        "id": "8",
-        "image": require("../assets/Giacomo-Boffo-Conversas-Barcelona-1.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-Conversas-Barcelona-1",
-        "description": "Poster campaign for series of events Conversas Barcelona",
-        "web": "false"
-    },
-    // {
-    //     "id": "9",
-    //     "image": require("../assets/Giacomo-Boffo-Fanny-Hagmeier-1.jpg"),
-    //     "url": "http://www.fannyhagmeier.com/",
-    //     "slug": "Giacomo-Boffo-Fanny-Hagmeier-1",
-    //     "description": "<a href='http://www.fannyhagmeier.com/' target='_blank'>Website</a>&nbsp; for German visual artist Fanny Hagmeier",
-    //     "web": "true"
-    // },
-    {
-        "id": "9",
-        "image": require("../assets/Giacomo-Boffo-Fanny-Hagmeier-1.jpg"),
-        "url": "http://www.fannyhagmeier.com/",
-        "slug": "Giacomo-Boffo-Fanny-Hagmeier-1",
-        "description": "Website for German visual artist Fanny Hagmeier",
-        "web": "true"
-    },
-    {
-        "id": "10",
-        "image": require("../assets/Giacomo-Boffo-Runroom.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-Runroom",
-        "description": "Logo for Barcelona-based tech & creative consultancy Runroom. At Folch",
-        "web": "false"
-    },
-    {
-        "id": "11",
-        "image": require("../assets/Giacomo-Boffo-St.Joost-Catalog-Transversal-Dialectics.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-St.Joost-Catalog-Transversal-Dialectics",
-        "description": "‘Transversal Dialectics’, 2015 Masters catalogue for AKV St.Joost Breda. With Marcello Jacopo Biffi, Alessandro Carosso",
-        "web": "false"
-    },
-    {
-        "id": "12",
-        "image": require("../assets/Giacomo-Boffo-Samizbar-1.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-Samizbar-1",
-        "description": "‘Samizbar e altre storie’, collection of short stories and poems by Turinese intellectual Vittorio Rieser. With Adrien Borderie",
-        "web": "false"
-    },
-    // {
-    //     "id": "13",
-    //     "image": require("../assets/Giacomo-Boffo-Ieke-Trinks-1.jpg"),
-    //     "url": "http://www.ieketrinks.nl/#/",
-    //     "slug": "Giacomo-Boffo-Ieke-Trinks-1",
-    //     "description": "<a href='http://www.ieketrinks.nl/#/' target='_blank'>Website</a>&nbsp; for Dutch performance artist Ieke Trinks",
-    //     "web": "true"
-    // },
-    {
-        "id": "13",
-        "image": require("../assets/Giacomo-Boffo-Ieke-Trinks-1.jpg"),
-        "url": "http://www.ieketrinks.nl/#/",
-        "slug": "Giacomo-Boffo-Ieke-Trinks-1",
-        "description": "Website for Dutch performance artist Ieke Trinks",
-        "web": "true"
-    },
-    {
-        "id": "14",
-        "image": require("../assets/Giacomo-Boffo-Secret-Art-Cinema.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-Secret-Art-Cinema",
-        "description": "Dynamic identity for Rotterdam independent video art festival Secret Art Cinema",
-        "web": "false"
-    },
-    {
-        "id": "15",
-        "image": require("../assets/Giacomo-Boffo-Marset-Catalogue-1.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-Marset-Catalogue-1",
-        "description": "2018/19 Marset Lighting catalogue. Illustration Cécile Gariepy, photo Dizzy Diaz. At Folch",
-        "web": "false"
-    },
-    {
-        "id": "16",
-        "image": require("../assets/Giacomo-Boffo-Romain-Du-Fou.jpg"),
-        "url": "",
-        "slug": "Giacomo-Boffo-Romain-Du-Fou",
-        "description": "Dynamic typeface Romain du Fou, redesign of 1700s French classic Romain du Roi",
-        "web": "false"
-    },
-]
+  {
+    id: "1",
+    image: require("../assets/Giacomo-Boffo-Podere-Tranche-1.jpg"),
+    url: "",
+    slug: "Podere Tranche",
+    description: "Brand Identity for Podere Tranche winery",
+    web: "false",
+  },
+  // {
+  //     "id": "2",
+  //     "image": require("../assets/GiacomoBoffo_NachoAlegre_1.jpg"),
+  //     "url": "http://nachoalegre.com/",
+  //     "slug": "Nacho Alegre",
+  //     "description": "<a href='http://nachoalegre.com/' target='_blank'>Website</a> &nbsp; for photographer Nacho Alegre. At Apartamento Studios.",
+  //     "web": "true"
+  // },
+  {
+    id: "2",
+    image: require("../assets/GiacomoBoffo_NachoAlegre_1.jpg"),
+    url: "http://nachoalegre.com/",
+    slug: "Nacho Alegre",
+    description:
+      "Website for photographer Nacho Alegre. At Apartamento Studios.",
+    web: "true",
+  },
+  {
+    id: "3",
+    image: require("../assets/GiacomoBoffo_BarcelonaDesignWeek_1.jpg"),
+    url: "",
+    slug: "Barcelona Design Week",
+    description:
+      "Campaign for Barcelona Design Week 2018. Illustration Bráulio Amado, photo Leonardo García Méndez. At Folch",
+    web: "false",
+  },
+  // {
+  //     "id": "4",
+  //     "image": require("../assets/Giacomo-Boffo-Brand-XY+.jpg"),
+  //     "url": "https://www.lascuolaopensource.xyz/en",
+  //     "slug": "Giacomo-Boffo-Brand-XY+",
+  //     "description": "Campaign for workshop ‘Un Brand per XY+’ at &nbsp;<a href='https://www.lascuolaopensource.xyz/en' target='_blank'> La Scuola Open Source</a>",
+  //     "web": "true"
+  // },
+  {
+    id: "4",
+    image: require("../assets/Giacomo-Boffo-Brand-XY+.jpg"),
+    url: "https://www.lascuolaopensource.xyz/en",
+    slug: "Giacomo-Boffo-Brand-XY+",
+    description:
+      "Campaign for workshop ‘Un Brand per XY+’ at La Scuola Open Source",
+    web: "true",
+  },
+  {
+    id: "5",
+    image: require("../assets/Giacomo-Boffo-Hermanos-Vinagre-1.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-Hermanos-Vinagre-1",
+    description:
+      "Brand identity for seafood tapas bar Hermanos Vinagre in Madrid. Illustrations Hunter French. At Apartamento",
+    web: "false",
+  },
+  {
+    id: "6",
+    image: require("../assets/Giacomo-Boffo-Manta-Poster.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-Manta-Poster",
+    description:
+      "Naming, logo and identity proposal for the former Manifattura Tabacchi in Cagliari. With Arianna Smaron",
+    web: "false",
+  },
+  {
+    id: "7",
+    image: require("../assets/Giacomo-Boffo-Valentina-Barucci-1.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-Valentina-Barucci-1",
+    description: "Campaign for exhibition ‘Valentina Barucci: Opere 1980-2020’",
+    web: "false",
+  },
+  {
+    id: "8",
+    image: require("../assets/Giacomo-Boffo-Conversas-Barcelona-1.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-Conversas-Barcelona-1",
+    description: "Poster campaign for series of events Conversas Barcelona",
+    web: "false",
+  },
+  // {
+  //     "id": "9",
+  //     "image": require("../assets/Giacomo-Boffo-Fanny-Hagmeier-1.jpg"),
+  //     "url": "http://www.fannyhagmeier.com/",
+  //     "slug": "Giacomo-Boffo-Fanny-Hagmeier-1",
+  //     "description": "<a href='http://www.fannyhagmeier.com/' target='_blank'>Website</a>&nbsp; for German visual artist Fanny Hagmeier",
+  //     "web": "true"
+  // },
+  {
+    id: "9",
+    image: require("../assets/Giacomo-Boffo-Fanny-Hagmeier-1.jpg"),
+    url: "http://www.fannyhagmeier.com/",
+    slug: "Giacomo-Boffo-Fanny-Hagmeier-1",
+    description: "Website for German visual artist Fanny Hagmeier",
+    web: "true",
+  },
+  {
+    id: "10",
+    image: require("../assets/Giacomo-Boffo-Runroom.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-Runroom",
+    description:
+      "Logo for Barcelona-based tech & creative consultancy Runroom. At Folch",
+    web: "false",
+  },
+  {
+    id: "11",
+    image: require("../assets/Giacomo-Boffo-St.Joost-Catalog-Transversal-Dialectics.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-St.Joost-Catalog-Transversal-Dialectics",
+    description:
+      "‘Transversal Dialectics’, 2015 Masters catalogue for AKV St.Joost Breda. With Marcello Jacopo Biffi, Alessandro Carosso",
+    web: "false",
+  },
+  {
+    id: "12",
+    image: require("../assets/Giacomo-Boffo-Samizbar-1.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-Samizbar-1",
+    description:
+      "‘Samizbar e altre storie’, collection of short stories and poems by Turinese intellectual Vittorio Rieser. With Adrien Borderie",
+    web: "false",
+  },
+  // {
+  //     "id": "13",
+  //     "image": require("../assets/Giacomo-Boffo-Ieke-Trinks-1.jpg"),
+  //     "url": "http://www.ieketrinks.nl/#/",
+  //     "slug": "Giacomo-Boffo-Ieke-Trinks-1",
+  //     "description": "<a href='http://www.ieketrinks.nl/#/' target='_blank'>Website</a>&nbsp; for Dutch performance artist Ieke Trinks",
+  //     "web": "true"
+  // },
+  {
+    id: "13",
+    image: require("../assets/Giacomo-Boffo-Ieke-Trinks-1.jpg"),
+    url: "http://www.ieketrinks.nl/#/",
+    slug: "Giacomo-Boffo-Ieke-Trinks-1",
+    description: "Website for Dutch performance artist Ieke Trinks",
+    web: "true",
+  },
+  {
+    id: "14",
+    image: require("../assets/Giacomo-Boffo-Secret-Art-Cinema.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-Secret-Art-Cinema",
+    description:
+      "Dynamic identity for Rotterdam independent video art festival Secret Art Cinema",
+    web: "false",
+  },
+  {
+    id: "15",
+    image: require("../assets/Giacomo-Boffo-Marset-Catalogue-1.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-Marset-Catalogue-1",
+    description:
+      "2018/19 Marset Lighting catalogue. Illustration Cécile Gariepy, photo Dizzy Diaz. At Folch",
+    web: "false",
+  },
+  {
+    id: "16",
+    image: require("../assets/Giacomo-Boffo-Romain-Du-Fou.jpg"),
+    url: "",
+    slug: "Giacomo-Boffo-Romain-Du-Fou",
+    description:
+      "Dynamic typeface Romain du Fou, redesign of 1700s French classic Romain du Roi",
+    web: "false",
+  },
+];
